d8017f5ae04d373116e58830404b7ed2a2650342
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-10 23:53:01 +0100

    Solution for the example 10

---
+++ ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo) {
+
+    var product = numOne * numTwo;
+
+    return product;
+
+}
+
+console.log(main(15, 3));
+console.log(main(25, 8));
+console.log(main(425, 10));
+
+module.exports = main;
